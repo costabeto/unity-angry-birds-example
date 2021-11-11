320a8469a18366bd78cf7e279d3a609db194daf3
Roberto Costa <roberto.araujo339@gmail.com>
2021-11-11 19:19:57 -0300

    Finished demo

---
+++ Assets/Enemy.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+public GameObject deathEffect;
+
+	public float health = 4f;
+
+	public static int EnemiesAlive = 0;
+
+	void Start ()
+	{
+		EnemiesAlive++;
+	}
+
+	void OnCollisionEnter2D (Collision2D colInfo)
+	{
+		if (colInfo.relativeVelocity.magnitude > health)
+		{
+			Die();
+		}
+	}
+
+	void Die ()
+	{
+		Instantiate(deathEffect, transform.position, Quaternion.identity);
+
+		EnemiesAlive--;
+		if (EnemiesAlive <= 0){
+
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+
+		Destroy(gameObject);
+	}
+}
